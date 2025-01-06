c3b262dc40e49efd8c4b57e6ee2f33129ff86e0b
rserradev <ronaldoserra1998@gmail.com>
2025-01-05 21:31:37 -0300

    first commit

---
+++ src/components/NewTask.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const newTask = ref('');
+
+const addTask = () => {
+    console.log(newTask.value);
+    newTask.value = '';
+}
+</script>
+
+<template>
+    <h1>task list</h1>
+    <input v-model="newTask" type="text" placeholder="Type new task"/>
+    <button @click="addTask"> Add </button>
+    <label > {{ newTask }}</label>
+</template>
