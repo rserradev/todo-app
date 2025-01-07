589b3d6feea044c2e0266896194808bd2c25b521
rserradev <ronaldoserra1998@gmail.com>
2025-01-07 07:59:55 -0300

    add task list to project

---
+++ docs.md
@@ -0,0 +1,18 @@
+# TODO App - Documentación
+
+## Funcionalidades por desarrollar
+- [ ] Mostrar lista de tareas.
+- [ ] Agregar nuevas tareas con un formulario.
+- [ ] Marcar tareas como completadas.
+- [ ] Crear componente `TaskItem`
+
+
+## Funcionalidades completadas
+- [x] Crear componente `NewTask`.
+
+
+## Notas técnicas
+- Usar `v-model` para enlazar datos entre el formulario y la lógica.
+- Usar `v-for` para recorrer array y mostrarlos por html
+- Considerar `computed` para filtrar tareas en el futuro.
+- 
--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -1,17 +1,43 @@
 <script setup>
 import { ref } from 'vue';
 
-const newTask = ref('');
+const newTask = ref(''); // Nueva Tarea
+const tasks = ref([]); // Lista de Tareas
 
-const addTask = () => {
-    console.log(newTask.value);
+const addTask = (name) => {
+    if (name === '') return;
+    tasks.value.push({
+        id: Date.now(),
+        name: name,
+        completed: false
+     });
     newTask.value = '';
 }
+
 </script>
 
 <template>
     <h1>task list</h1>
-    <input v-model="newTask" type="text" placeholder="Type new task"/>
+
+    <!-- Input para añadir una nueva tarea -->
+    <input
+        v-model="newTask"
+        type="text"
+        placeholder="Type new task"
+    />
+
+    <!-- Botón para añadir la tarea -->
     <button @click="addTask"> Add </button>
+
+    <!-- Mostrar el texto de la nueva tarea -->
     <label > {{ newTask }}</label>
+
+    <!-- Lista de tareas -->
+    <ul>
+        <li
+            v-for="task in tasks"
+            :key="task.id">
+                {{ task }}
+        </li>
+    </ul>
 </template>
